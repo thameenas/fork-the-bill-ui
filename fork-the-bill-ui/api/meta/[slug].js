05631717a1cbfafc07f27f63a1e8fa808bdafe3c
thameena <thameena8@gmail.com>
2025-10-21 00:33:03 +0530

    Use vercel rewrites

--- fork-the-bill-ui/api/meta/[slug].js
+++ fork-the-bill-ui/api/meta/[slug].js
@@ -151,7 +151,7 @@ function generateDefaultHTML(baseUrl) {
 </html>`;
 }
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   const { slug } = req.query;
   
   if (!slug) {
